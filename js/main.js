94a4329d1637673591d5462c249e23fe5bab6a32
Ilya <43725229+sadiolem@users.noreply.github.com>
2021-08-18 18:18:03 +0300

    Refactoring

--- js/main.js
+++ js/main.js
@@ -1,38 +1,53 @@
-const startButton = document.querySelector('.start-btn');
-const boxes = document.querySelectorAll('.box');
-const textarea = document.querySelector('textarea');
-
-const animateBoxes = () => {
-	startButton.removeEventListener('click', animateBoxes);
-
-	startButton.textContent = "in progress...";
-	textarea.textContent = `--PROGRESS START-- \n`;
-
-	boxes.forEach((box, index) => {
-		setTimeout(() => {	
-			box.classList.toggle('box-down');
-		}, index * 500);
-		
-		box.addEventListener("transitionstart", () => {
- 			textarea.textContent += `Box ${index+1} animation start \n`;
- 		});
-		
- 		box.addEventListener("transitionend", () => {
- 			textarea.textContent += `Box ${index+1} animation end \n`;
- 		});
-	});
-
-	let time1 = boxes.length * 650;
-	let time2 = time1 + 600;
-
-	setTimeout(() => {
- 		textarea.textContent += "--PROGRESS END--";
- 	}, time1);
-
- 	setTimeout(() => {
-		alert('Success!');
-		location.reload();
-	}, time2);
-}
-
-startButton.addEventListener('click', animateBoxes);
+document.addEventListener('DOMContentLoaded', () => {
+  const startButton = document.querySelector('.start-btn');
+  const textarea = document.querySelector('textarea');
+  
+  startButton.addEventListener('click', init);
+  
+  function init() {
+    startButton.removeEventListener('click', init);
+  
+    startButton.textContent = "in progress...";
+    textarea.textContent = "--PROGRESS START--" + '\n';
+  
+    const boxes = document.querySelectorAll('.box');
+  
+    animateBoxes(boxes);
+  
+    const timerForProgress = boxes.length * 650;
+    const timerForEnd = timerForProgress + 600;
+  
+    setTimeout(() => {
+      textarea.textContent += "--PROGRESS END--";
+    }, timerForProgress);
+  
+    reinit(timerForEnd);
+  }
+  
+  function animateBoxes(boxes) {
+    boxes.forEach((box, index) => {
+      setTimeout(() => {
+        box.classList.toggle('box-down');
+      }, index * 500);
+  
+      showLogs(box, index);
+    });
+  }
+  
+  function showLogs(box, index) {
+    box.addEventListener("transitionstart", () => {
+      textarea.textContent += `Box ${index+1} animation start \n`;
+    });
+  
+    box.addEventListener("transitionend", () => {
+      textarea.textContent += `Box ${index+1} animation end \n`;
+    });
+  }
+  
+  function reinit(timer) {
+    setTimeout(() => {
+      alert('Success!');
+      location.reload();
+    }, timer);
+  }
+});
